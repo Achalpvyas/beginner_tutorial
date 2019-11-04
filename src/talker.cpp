a0bfe2750f941492587233f5c2912d8b015c77da
Achalpvyas <achalpvyas@gmail.com>
2019-11-04 04:14:30 -0500

    Updating talker

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,76 +1,105 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/**Copyright <2019> <Achal Pragneshkumar Vyas>
+ *Copyright <YEAR> <COPYRIGHT HOLDER>
 
+*Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are *met:
+
+*1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+*2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the *documentation and/or other materials provided with the distribution.
+
+*3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this *software without specific prior written permission.
+
+*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT *LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT *HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT *LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON *ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE *USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+/**
+ * @file 		  talker.cpp
+ * @author 		Achal Vyas
+ * @copyright BSD-3
+ */
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
 #include <sstream>
+#include "beginner_tutorials/change_string_output.h"
 
+extern std::string text = "My name is Achal Vyas ";
+
+/**
+ * @brief a function that changes the stream message
+ * @param req represents the data being sent to the service
+ * @param res represents the data being provided by the client
+ * @return bool
+ */
+bool newtext(beginner_tutorials::change_string_output::Request &req,
+                beginner_tutorials::change_string_output::Response &res) {
+  text = req.originaltext;
+  ROS_INFO_STREAM("The string is changed to the new string: ");
+  res.newtext = req.originaltext;
+  return true;
+}
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+   * The ros::init() function initializes the talker node
+   */ 
   ros::init(argc, argv, "talker");
 
+  // Using a variable to store freq
+  int freq = 2;
+
   /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
+   * NodeHandle maintains the communication with ROS.
    */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Rate loop_rate(freq);
+  // Let's generate log messages
+  if (argc > 1) {
+    // Converting string argument to integer
+    freq = atoi(argv[1]);
 
-  ros::Rate loop_rate(10);
+    if (freq < 0) {
+      // For negative frequency or less than zero
+      ROS_FATAL_STREAM("Frequency cannot be less than zero");
+      ros::shutdown();
+      return 1;
+    } else if (freq == 0) {
+      // For frequency equals to '0'
+      ROS_ERROR_STREAM("Frequency cannot be zero!!");
+      ROS_INFO_STREAM("Changing frequency to 4hz");
+      freq = 4;
+    } else if (freq >= 10) {
+      // For frequency greater than 10
+      ROS_WARN_STREAM("very fast to read!!");
+      ROS_INFO_STREAM("Changing frequency to 5hz");
+      freq = 5;
+    }
+  }
+  ros::Publisher chatter_pub = n.advertise < std_msgs::String
+      > ("chatter", 1000);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
+  // For service to changeBaseOutputString
+  ros::ServiceServer server = n.advertiseService("change_string_output",
+                                                 newtext);
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
+    ROS_DEBUG_STREAM_ONCE("Current frequency: " << freq);
+
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "My name is achal " << count;
+    ss << text;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
 
     /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
+     * The publish() function represents the way of sending messages.
      */
     chatter_pub.publish(msg);
 
@@ -79,7 +108,6 @@ int main(int argc, char **argv)
     loop_rate.sleep();
     ++count;
   }
-
 
   return 0;
 }
